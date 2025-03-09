d1b4648647ec64122fc46b2d0520877c8385f344
EmreCandan <emreicandan@gmail.com>
2025-03-10 00:36:28 +0300

    feat : add missing dependencies

--- src/modules/parent/parent.module.ts
+++ src/modules/parent/parent.module.ts
@@ -1,15 +1,18 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ParentSchema } from "src/schemas/parent.schema";
+import { ParentController } from "./parent.controller";
+import { ParentService } from "./parent.service";
+import { ParentRepository } from "./parent.repository";
 
 @Module({
     imports: [MongooseModule.forFeature([{
         name: 'Parents',
         schema: ParentSchema
     }])],
-    controllers: [],
-    providers: [],
-    exports: []
+    controllers: [ParentController],
+    providers: [ParentService, ParentRepository],
+    exports: [ParentService, ParentRepository]
 })
 
 export class ParentModule { };
--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -1,15 +1,18 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { StudentSchema } from "src/schemas/student.schema";
+import { StudentService } from "./student.service";
+import { StudentRepository } from "./student.repository";
+import { StudentController } from "./student.controller";
 
 @Module({
     imports: [MongooseModule.forFeature([{
         name: 'Students',
         schema: StudentSchema
     }])],
-    controllers: [],
-    providers: [],
-    exports: []
+    controllers: [StudentController],
+    providers: [StudentService,StudentRepository],
+    exports: [StudentService,StudentRepository]
 })
 
 export class StudentModule { };
